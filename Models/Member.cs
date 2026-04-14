1568826b27e2a4a372e1eb253375163609e05050
Yuki.kaco <a0936660023@gmail.com>
2026-04-14 15:05:35 +0800

    新增Member Class

---
+++ Models/Member.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DatabaseQuiz.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "身分證字號")]
+        public string UserId { get; set; }
+
+        [Required]
+        [Display(Name = "手機門號")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "是否為管理員")]
+        public bool isAdmin { get; set; }
+    }
+}
